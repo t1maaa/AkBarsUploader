c930a8228bed93024a8e3fbf5317b5da90a5b699
t1maaa <t1maaa@mail.ru>
2021-01-31 18:37:06 +0700

    init

---
+++ AkBarsUploader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AkBarsUploader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (IHost host = CreateHostBuilder(args).Build())
+            {
+                await host.RunAsync();
+            }
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(
+                (hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+                    
+                    IHostEnvironment env = hostingContext.HostingEnvironment;
+
+                    configuration
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+                        .AddCommandLine(args);
+                });
+    }
+}
