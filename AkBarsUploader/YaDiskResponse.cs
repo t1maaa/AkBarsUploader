7d2dfbade7032d5104c406e87014a955742be2c5
t1maaa <t1maaa@mail.ru>
2021-02-02 00:13:51 +0700

    YaDiskResponse.cs

---
+++ AkBarsUploader/YaDiskResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace AkBarsUploader
+{
+    public class YaDiskResponse
+    {
+        public string Href { get; set; }
+        public string Method { get; set; }
+        public bool Templated { get; set; }
+        public string Message { get; set; }
+        public string Description { get; set; }
+        public string Error { get; set; }
+    }
+}
